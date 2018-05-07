63455dceafedcdf4e443ca1feb3c7cee660cc8ae
Kris Neuharth <kris.neuharth@gmail.com>
2018-05-07 10:33:33 -0700

    Add some tests

---
+++ tests/test_cpv.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+
+from cpv.cpv import Spot, Metadata
+
+
+class TestCPV(TestCase):
+
+    def test_get_rotations_data(self):
+        pass
+
+
+    def test_get_spots_data(self):
+        pass
+
+
+    def test_filter_sum(self):
+        pass
+
+
+    def test_calc_cpv(self):
+        pass
+
+
+    def test_calculate(self):
+        pass
+
+
+    def test_cpv_by_creative(self):
+        pass
+
+
+    def test_cpv_by_rotation(self):
+        pass
+
+
+    def test_cpv_by_day(self):
+        pass
+
+
+    def test_cpv_by_rotation_by_day(self)
+        pass
+
+
+    def test_cpv_by_rotation_by_creative_by_day(self):
+        pass
+
+
---
+++ tests/test_data.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+from cpv.cpv import Spot, Metadata
+
+
+class TestSpot(TestCase):
+
+    def test_create(self):
+        spot = Spot()
+        spot.date = "01/02/2016"
+        spot.time = "8:30 AM"
+        spot.creative = "TEST001H"
+        spot.spend = 120.50
+        spot.views = 100
+        spot.rotation = 'Morning'
+
+        self.assertTrue(spot.rotation == 'Morning')
+
+    def test_repr(self):
+        spot = Spot()
+        spot.date = "01/02/2016"
+        spot.time = "8:30 AM"
+        spot.creative = "TEST001H"
+        spot.spend = 120.50
+        spot.views = 100
+        spot.rotation = 'Morning'
+
+        str_spot = str(spot)
+
+        self.assertTrue('"rotation": "Morning"' in str_spot)
+
+
+class TestMetadata(TestCase):
+
+    def test_create(self):
+        meta = Metadata()
+
+        meta.rotations_names = ['Morning', 'Afternoon']
+        meta.creative_names = ['TEST1', 'TEST2']
+        meta.spot_days = [1, 2, 3]
+
+        self.assertTrue(len(meta.rotations_names) == 2)
+        self.assertTrue(len(meta.creative_names) == 2)
+        self.assertTrue(len(meta.spot_days) == 3)
